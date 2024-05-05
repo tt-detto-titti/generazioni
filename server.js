3d31323897cc2372e0553c9ce9ed849b48bd6cec
Tommaso Tamagnini <36273703+tt-detto-titti@users.noreply.github.com>
2024-05-05 15:52:51 +0200

    Aggiunta l'implementazione di swagger-jsdoc e swagger-ui-express

--- server.js
+++ server.js
@@ -2,24 +2,34 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+
 const app = express();
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "GenerAzioni API",
+      version: "1.0.0",
+      description: "Queste sono le API del progetto GenerAzioni.",
+    },
+  },
+  apis: ["./app/routes/*.js"], // files containing annotations as above
+};
+const swaggerDocument = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 var corsOptions = {
   origin: "http://localhost:8081",
 };
-
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
-
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
-});
 
 const db = require("./app/models");
 db.mongoose
@@ -38,7 +48,6 @@ db.mongoose
 require("./app/routes/richiesta.routes.js")(app);
 require("./app/routes/utente.routes.js")(app);
 
-// set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
