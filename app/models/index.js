f6b50ba45201e440e8226454d5243366d214f060
Tommaso Tamagnini <36273703+tt-detto-titti@users.noreply.github.com>
2024-05-06 18:54:37 +0200

    Unita la configurazione del database in un file solo

--- app/models/index.js
+++ app/models/index.js
@@ -1,11 +1,14 @@
-const dbConfig = require("../config/db.config.js");
-
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 const db = {};
 db.mongoose = mongoose;
-db.url = dbConfig.url;
+
+db.url = "mongodb+srv://generazioni.e4br1v2.mongodb.net/?retryWrites=true&w=majority&appName=generazioni";
+db.name = "generazioni";
+db.user = "dev";
+db.pass = "dev";
+
 db.richieste = require("./richiesta.model.js")(mongoose);
 db.utenti = require("./utente.model.js")(mongoose);
 
