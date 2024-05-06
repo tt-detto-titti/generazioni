6111e4dacfbbd5508012fc33f00f7f9712f14833
Tommaso Tamagnini <36273703+tt-detto-titti@users.noreply.github.com>
2024-05-06 20:43:23 +0200

    Aggiornata l'API per il signup

--- app/controllers/utente.controller.js
+++ app/controllers/utente.controller.js
@@ -3,31 +3,26 @@ const Utente = db.utenti;
 
 // Crea e salva un nuovo utente
 exports.signup = (req, res) => {
-  // Validazione
-  if (
-    !req.body.nome ||
-    !req.body.cognome ||
-    !req.body.email ||
-    !req.body.password
-  ) {
-    res.status(400).send({ message: "Tutti i campi sono obbligatori." });
-  }
-
   const utente = new Utente({
     nome: req.body.nome,
     cognome: req.body.cognome,
+    cf: req.body.cf,
+    dataNascita: req.body.dataNascita,
+    residenza: req.body.residenza,
+    telefono: req.body.telefono,
     email: req.body.email,
     password: req.body.password,
+    qualifica: ["utente"],
   });
 
   utente
     .save(utente)
     .then((data) => {
-      res.send(data);
+      res.status(200).send(data);
     })
     .catch((err) => {
-      res.statuss(500).send({
-        message: err.message || "Impossibile salvare l'utente nel database.",
+      res.status(500).send({
+        message: "Impossibile salvare l'utente: " + err.message,
       });
     });
 };
--- app/routes/utente.routes.js
+++ app/routes/utente.routes.js
@@ -1,18 +1,11 @@
 module.exports = (app) => {
   const utenti = require("../controllers/utente.controller.js");
-
   var router = require("express").Router();
 
-  /**
-   * @openapi
-   * /:
-   *  post:
-   *      summary: crea un utente
-   */
-  router.post("/", utenti.signup);
-
+  // Registra un utente
+  router.post("/register", utenti.signup);
   // Logga un utente
-  router.get("/", utenti.login);
+  router.post("/login", utenti.login);
 
   app.use("/api/utenti", router);
 };
