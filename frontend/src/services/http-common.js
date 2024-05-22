cb7a0bb7f548fa1e9fbfc198e5eff285d3d674d0
Tommaso Tamagnini <36273703+tt-detto-titti@users.noreply.github.com>
2024-05-22 23:51:06 +0200

    Aggiornate views

--- frontend/src/services/http-common.js
+++ frontend/src/services/http-common.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  // TODO cambiare sta cosa poco elegante
+  baseURL: "https://generazioni.onrender.com/api",
+  // baseURL: "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json",
   },
