353048a9d9cbffe503c6c23a7192b6e6060425fb
Tommaso Tamagnini <36273703+tt-detto-titti@users.noreply.github.com>
2024-05-23 10:09:15 +0200

    Modificato il meccanismo di production

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const path = __dirname + "/app/views/";
+const path = __dirname + "/../frontend/dist/";
 
 app.use(express.static(path));
 
--- frontend/src/services/http-common.js
+++ frontend/src/services/http-common.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  // TODO cambiare sta cosa poco elegante
-  baseURL: "https://generazioni.onrender.com/api",
-  // baseURL: "http://localhost:8080/api",
+  baseURL:
+    import.meta.env.VITE_API_URL || "https://generazioni.onrender.com/api",
   headers: {
     "Content-type": "application/json",
   },
