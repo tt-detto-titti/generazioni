1327557f2ef9b2ebb9623fa5d44b9aba9a6198df
Tommaso Tamagnini <36273703+tt-detto-titti@users.noreply.github.com>
2024-05-25 19:11:59 +0200

    Aggiornate le views

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
 import Vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
-import Vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+import Vuetify from "vite-plugin-vuetify";
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    Vue({
-      template: { transformAssetUrls },
-    }),
-    Vuetify(),
-    Components(),
-  ],
+  plugins: [Vue(), Vuetify(), Components()],
   root: "frontend",
   resolve: {
     alias: {
