b2523117ce7b956d1d229f33a731cc9d25f401da
Tommaso Tamagnini <36273703+tt-detto-titti@users.noreply.github.com>
2024-06-10 19:23:52 +0200

    Riuniti test login e corretti gli errori

--- test/login.test.js
+++ test/login.test.js
@@ -1,21 +1,76 @@
-//test case 10
-const app = require('./app')
-const mongoose = require('./mongoose')
-it("test login", async(done) => {
-  //login di un utente
-  const response = await request(app)
-   .post("/api/auth/login")
-   .send({
-      email: "mario.rossi@email.com",
-      password: "pastaalpesto",
-    })
-   .set("Accept", "application/json")
-   .expect("Content-Type", /json/)
-   .expect(200);
-  expect(response.status).toBe(200);
-  expect(response.body.isSuccesful).toBe(true);
-  expect(response.body.utente.email).toBe("mario.rossi@email.com");
-  expect(response.body.utente.ruoli[0]._id).toBeDefined();
-  expect(response.body.accessToken).toBeDefined();
-  done();
-})
+const request = require('supertest');
+const app = require('../backend/server');
+
+describe('Test Login (3)', () => {
+  // test case 10
+  it('Login di un utente con email e password corretta', async () => {
+    const response = await request(app)
+      .post('/api/auth/login')
+      .send({
+        email: 'giovanni.rana@email.it',
+        password: 'pastaalpesto'
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(response.body.email).toBe('giovanni.rana@email.it');
+    expect(response.body.ruoli[0]).toBeDefined();
+    expect(response.body.accessToken).toBeDefined();
+  });
+
+  // test case 11
+  it('Login di un utente con email corretta ma password sbagliata', async () => {
+    const response = await request(app)
+      .post('/api/auth/login')
+      .send({
+        email: 'giovanni.rana@email.it',
+        password: 'pastaalsugo'
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(401);
+    expect(response.body.message).toBe('Password non valida!');
+  });
+
+  // test case 12
+  it('Login di un utente con una email non registrata del database', async () => {
+    const response = await request(app)
+      .post('/api/auth/login')
+      .send({
+        email: 'giuseppe.bianchi@email.com',
+        password: 'pastaalpesto'
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404);
+    expect(response.body.message).toBe('Utente non trovato!');
+  });
+
+  // test case 13
+  it('Login di un utente con password vuota', async () => {
+    const response = await request(app)
+      .post('/api/auth/login')
+      .send({
+        email: 'giovanni.rana@email.it',
+        password: ''
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400);
+    expect(response.body.message).toBe('È necessario inserire la password!');
+  });
+
+  // test case 14
+  it('Login di utente con email vuota', async () => {
+    const response = await request(app)
+      .post('/api/auth/login')
+      .send({
+        email: '',
+        password: 'pastaalpesto'
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400);
+    expect(response.body.message).toBe("È necessario inserire l'email!");
+  });
+});
