9194452e2884add737e9f120df0f2bc224d0a8f6
Tommaso Tamagnini <36273703+tt-detto-titti@users.noreply.github.com>
2024-06-10 19:23:29 +0200

    Aggiunto test app

---
+++ test/app.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const app     = require('../backend/server');
+
+test('app module should be defined', () => {
+  expect(app).toBeDefined();
+});
+
+test('GET / should return 200', () => {
+  return request(app)
+    .get('/')
+    .expect(200);
+});
