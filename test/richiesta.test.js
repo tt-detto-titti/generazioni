19af43ec1f02f6e5bcb4c703b5987aa9854d2dd3
Tommaso Tamagnini <36273703+tt-detto-titti@users.noreply.github.com>
2024-06-11 15:50:31 +0200

    Aggiunti test di accettazione delle richieste

--- test/richiesta.test.js
+++ test/richiesta.test.js
@@ -3,9 +3,13 @@ const app = require('../backend/server');
 const jwt = require('jsonwebtoken');
 const config = require('../backend/config/auth.config');
 
+const ID_ANZIANO = '6667169dfc463448ca75ea9b';
+const ID_VOLONTARIO = '6653382801e34c3fd8142996';
+const ID_RICHIESTA_VALIDA = '66681fbb58d261d2347505ab';
+const ID_RICHIESTA_NON_VALIDA = '6667169dfc463448ca75ea9b';
+
 var token;
 function generaToken(id) {
-  // Genero il token d'accesso per l'account di test
   return jwt.sign({ id: id }, config.JWT_SECRET, {
     algorithm: 'HS256',
     expiresIn: 86400
@@ -15,8 +19,8 @@ function generaToken(id) {
 describe('Test - (6) NuovaRichiesta', () => {
   // Test Case 1
   test('Aggiunta di una nuova richiesta con i campi validi, l\'accessToken valido e disponendo del ruolo "anziano"', async () => {
-    token = generaToken('666812b5775238144c44b0d8');
-    const response = await request(app)
+    token = generaToken(ID_ANZIANO);
+    const res = await request(app)
       .post('/api/matchmaker/richieste/add')
       .set('x-access-token', token)
       .set('Accept', 'application/json')
@@ -28,13 +32,13 @@ describe('Test - (6) NuovaRichiesta', () => {
       })
       .expect('Content-Type', /json/)
       .expect(201);
-    expect(response.body.message).toBe('La richiesta è stata salvata correttamente.');
+    expect(res.body.message).toBe('La richiesta è stata salvata correttamente.');
   }, 10000);
 
   // Test Case 2
   test('Aggiunta di una nuova richiesta con i campi validi, l\'accessToken valido e disponendo di un ruolo diverso da "anziano"', async () => {
-    token = generaToken('6653382801e34c3fd8142996');
-    const response = await request(app)
+    token = generaToken(ID_VOLONTARIO);
+    const res = await request(app)
       .post('/api/matchmaker/richieste/add')
       .set('x-access-token', token)
       .set('Accept', 'application/json')
@@ -44,7 +48,114 @@ describe('Test - (6) NuovaRichiesta', () => {
         descrizione: 'ho bisogno di aiuto per fare la spesa',
         categoria: 'aiuto fuori casa'
       })
+      .expect('Content-Type', /json/)
       .expect(403);
-          expect(response.body.message).toBe('Richiede il ruolo di anziano!');
+    expect(res.body.message).toBe('Richiede il ruolo di anziano!');
   }, 10000);
+
+  // Test Case 15
+  test('Aggiunta di una nuova richiesta con i campi validi a parte data che è vuoto, l\'accessToken valido e disponendo del ruolo "anziano"', async () => {
+    token = generaToken(ID_ANZIANO);
+    const res = await request(app)
+      .post('/api/matchmaker/richieste/add')
+      .set('x-access-token', token)
+      .set('Accept', 'application/json')
+      .send({
+        durata: 90,
+        descrizione: 'ho bisogno di aiuto per fare la spesa',
+        categoria: 'aiuto fuori casa'
+      })
+      .expect('Content-Type', /json/)
+      .expect(400);
+    expect(res.body.message).toBe('È necessario inserire la data!');
+  }, 10000);
+
+  // Test Case 30
+  test('Aggiunta di una nuova richiesta con i campi validi a parte durata che è vuoto, l\'accessToken valido e disponendo del ruolo "anziano"', async () => {
+    token = generaToken(ID_ANZIANO);
+    const res = await request(app)
+      .post('/api/matchmaker/richieste/add')
+      .set('x-access-token', token)
+      .set('Accept', 'application/json')
+      .send({
+        data: '2025-07-08T12:00:00Z',
+        descrizione: 'ho bisogno di aiuto per fare la spesa',
+        categoria: 'aiuto fuori casa'
+      })
+      .expect('Content-Type', /json/)
+      .expect(400);
+    expect(res.body.message).toBe('È necessario inserire la durata!');
+  }, 10000);
+
+  // Test Case 31
+  test('Aggiunta di una nuova richiesta con i campi validi a parte descrizione che è vuoto, l\'accessToken valido e disponendo del ruolo "anziano"', async () => {
+    token = generaToken(ID_ANZIANO);
+    const res = await request(app)
+      .post('/api/matchmaker/richieste/add')
+      .set('x-access-token', token)
+      .set('Accept', 'application/json')
+      .send({
+        data: '2025-07-08T12:00:00Z',
+        durata: 90,
+        categoria: 'aiuto fuori casa'
+      })
+      .expect('Content-Type', /json/)
+      .expect(400);
+    expect(res.body.message).toBe('È necessario inserire una descrizione!');
+  }, 10000);
+
+  // Test Case 32
+  test('Aggiunta di una nuova richiesta con i campi validi a parte categoria che è vuoto, l\'accessToken valido e disponendo del ruolo "anziano"', async () => {
+    token = generaToken(ID_ANZIANO);
+    const res = await request(app)
+      .post('/api/matchmaker/richieste/add')
+      .set('x-access-token', token)
+      .set('Accept', 'application/json')
+      .send({
+        data: '2025-07-08T12:00:00Z',
+        durata: 90,
+        descrizione: 'ho bisogno di aiuto per fare la spesa'
+      })
+      .expect('Content-Type', /json/)
+      .expect(400);
+    expect(res.body.message).toBe('È necessario inserire la categoria di aiuto!');
+  }, 10000);
+});
+
+describe('Test - (13) VisualizzazioneRichieste', () => {
+  // Test Case 16
+  test("Accettazione di una richiesta d'aiuto da parte di un volontario", async () => {
+    token = generaToken(ID_VOLONTARIO);
+    const res = await request(app)
+      .put(`/api/matchmaker/richieste/accetta/${ID_RICHIESTA_VALIDA}`)
+      .set('x-access-token', token)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(res.body.message).toBe('La richiesta è stata accettata correttamente.');
+  }, 10000);
+
+  // Test Case 17
+  test('Utente non registrato come volontario prova ad accettare una richiesta', async () => {
+    token = generaToken(ID_ANZIANO);
+    const res = await request(app)
+      .put(`/api/matchmaker/richieste/accetta/${ID_RICHIESTA_VALIDA}`)
+      .set('x-access-token', token)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(403);
+    expect(res.body.message).toBe('Richiede il ruolo di volontario!');
+  }, 10000);
+
+  // Test Case 18
+  test('Volontario accetta richiesta che non esiste', async () => {
+    token = generaToken(ID_VOLONTARIO);
+    const res = await request(app)
+      .put(`/api/matchmaker/richieste/accetta/${ID_RICHIESTA_NON_VALIDA}`)
+      .set('x-access-token', token)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404);
+    expect(res.body.message).toBe('Richiesta non trovata!');
+  });
 });
