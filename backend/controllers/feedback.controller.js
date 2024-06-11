070b1084aa0f98da60a0e269c0ae7dc6c9005793
Tommaso Tamagnini <36273703+tt-detto-titti@users.noreply.github.com>
2024-06-11 22:00:35 +0200

    Aggiunta validazione dei dati

--- backend/controllers/feedback.controller.js
+++ backend/controllers/feedback.controller.js
@@ -1,4 +1,4 @@
-const db = require("../models");
+const db = require('../models');
 const Feedback = db.feedback;
 const Richiesta = db.richiesta;
 const Utente = db.utente;
@@ -8,42 +8,46 @@ const yup = require('yup');
 const schemaFeedback = yup.object().shape({
   tipologia: yup.string().required('È necessario inserire la tipologia!'),
   descrizione: yup.string().required('È necessario inserire la descrizione!')
-})
+});
 
 exports.nuovoFeedback = async (req, res) => {
   try {
+    await schemaFeedback.validate({
+      tipologia: req.body.tipologia,
+      descrizione: req.body.descrizione
+    });
+
     const richiesta = await Richiesta.findById(req.params.id_richiesta);
     if (!richiesta) {
-      res.status(404).send({ message: "Richiesta non trovata!" });
-      return;
+      return res.status(404).send({ message: 'Richiesta non trovata!' });
     }
 
     const utente = await Utente.findById(req.id_utente);
     if (!utente) {
-      res.status(404).send({ message: "Utente non trovato!" });
-      return;
+      return res.status(404).send({ message: 'Utente non trovato!' });
     }
 
     const feedback = new Feedback({
       id_utente: utente._id,
       id_richiesta: richiesta._id,
-      destinazione: "",
+      destinazione: '',
       tipologia: req.body.tipologia,
-      descrizione: req.body.descrizione,
+      descrizione: req.body.descrizione
     });
 
-    if (utente.ruoli.includes("anziano")) {
-      feedback.destinazione = "per volontario";
+    if (utente.ruoli.includes('anziano')) {
+      feedback.destinazione = 'per volontario';
     }
-    if (utente.ruoli.includes("volontario")) {
-      feedback.destinazione = "per anziano";
+    if (utente.ruoli.includes('volontario')) {
+      feedback.destinazione = 'per anziano';
     }
 
     await feedback.save();
-    res.status(201).send({ message: "Feedback salvato con successo." });
+    return res.status(201).send({ message: 'Feedback salvato con successo.' });
   } catch (err) {
-    res
-      .status(500)
-      .send({ message: "Impossibile salvare il feedback: " + err });
+    if (err instanceof yup.ValidationError) {
+      return res.status(400).send({ message: err.message });
+    }
+    return res.status(500).send({ message: 'Impossibile salvare il feedback: ' + err });
   }
 };
